aab351e79909f6561c1b0e612f2652a4f93be61c
Alessandro Mariotti <alessandro.mariotti@zupit.it>
2021-04-17 18:57:59 +0200

    Add admin models

--- drf_file_upload/admin.py
+++ drf_file_upload/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from drf_file_upload import models
+
+admin.site.register(models.AuthenticatedUploadedFile)
+admin.site.register(models.AnonymousUploadedFile)

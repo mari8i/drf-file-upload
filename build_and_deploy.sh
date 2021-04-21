83bab708643a9f87bc9a3f41ee95d1b1d74e584d
Alessandro Mariotti <alessandro.mariotti@zupit.it>
2021-04-21 08:02:04 +0200

    Complete deploy script

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,3 +1,10 @@
+VERSION="$1"
+perl -pi -e "s/version = .*/version = $VERSION/" setup.cfg
 python setup.py sdist bdist_wheel
 python -m twine check dist/*
 python -m twine upload dist/*
+git add setup.cfg
+git commit -m "Bump version $VERSION"
+git tag $VERSION
+git push
+git push --tags
